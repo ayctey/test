4b12df0735456edf596e75e58d797321bab695cf
ayctey <ayctey@aycteydeMacBook-Pro.local>
2015-06-14 09:40:17 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by ayctey on 15-6-14.
+//  Copyright (c) 2015年 ayctey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by ayctey on 15-6-14.
+//  Copyright (c) 2015年 ayctey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
